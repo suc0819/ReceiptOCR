b6d601e580c63b364dd05d5c5850dadebda2f2bf
suc0819 <suc0819@gmail.com>
2026-06-07 07:14:20 -0700

    add OCRService

---
+++ ReceiptOCR/ReceiptOCR/Services/OCRService.swift
@@ -0,0 +1,30 @@
+//
+//  OCRService.swift
+//  ReceiptOCR
+//
+//  Created by h2171435 on 2026/06/07.
+//
+
+import UIKit
+import Vision
+
+class OCRService {
+    
+    func recognize(image: UIImage, completion: @escaping (String) -> Void) {
+        guard let cgImage = image.cgImage else { return }
+        
+        let request = VNRecognizeTextRequest { request, error in
+            let observations = request.results as? [VNRecognizedTextObservation]
+            let text = observations?
+                .compactMap { $0.topCandidates(1).first?.string }
+                .joined(separator: "\n") ?? ""
+            completion(text)
+        }
+        
+        request.recognitionLanguages = ["ko-KR", "en-US"]
+        request.recognitionLevel = .accurate
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage)
+        try? handler.perform([request])
+    }
+}
